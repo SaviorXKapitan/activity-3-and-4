0b2411b155057dc77e6b0140b946c7cd246cc6e4
BryanBucu <75920081+BryanBucu@users.noreply.github.com>
2020-12-13 11:27:51 +0800

    Update Fraction.java

--- PopStack [Act3]/src/Fraction.java
+++ PopStack [Act3]/src/Fraction.java
@@ -1,8 +1,19 @@
-
-
 /**
  * This class contains various methods that can be used to perform arithmetic operations on fractions.
  */
 
 public class Fraction {
-}
+    /**
+     * Holds the value of this fraction's numerator.
+     */
+    private int numerator;
+    /**
+     * Holds the value of this fraction's denominator.
+     */
+    private int denominator;
+
+    /**
+     * Constructs a new Fraction with numerator = 0 and denominator = 1, a fraction with a value equivalent to 0.
+     **/
+    public Fraction() {
+        numerator = 0;
